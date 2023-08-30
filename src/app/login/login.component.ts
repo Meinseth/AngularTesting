2d62d159cb73242dd99b2fcf993d04af587d7580
Vebjørn Meinseth <vebjorn@moonde.no>
2023-08-30 21:28:53 +0200

    Angular 16

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,13 @@
-import { Component, type OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  standalone: true,
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
   ngOnInit (): void {
   }
+  
 }
