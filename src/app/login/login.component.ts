9cf50f40ced8187d3e1efe55897adcebad7b8856
Vebjørn Meinseth <vebjorn@moonde.no>
2023-08-30 21:16:17 +0200

    Setting up routes, guard and service

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component, type OnInit } from '@angular/core'
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  ngOnInit (): void {
+  }
+}
